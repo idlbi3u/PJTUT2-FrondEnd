c07c2500438ba0ac21f10bb88e782163bfafb803
idlbi3u <idlbi3u@etu.univ-lorraine.fr>
2022-03-20 11:45:20 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PJTUT2-FrontEnd',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
