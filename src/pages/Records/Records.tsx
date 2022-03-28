08d018dc278ed0ecd4536035b40ff55472b3fccf
Selim1223 <91537143+Selim1223@users.noreply.github.com>
2022-03-28 19:29:45 +0200

    new commit

--- src/pages/Records/Records.tsx
+++ src/pages/Records/Records.tsx
@@ -1,6 +1,7 @@
-import { IonCol, IonContent, IonGrid, IonHeader, IonItem, IonPage, IonRow, IonSearchbar, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonPage, IonRow, IonSearchbar, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import {   pencil, trash,  } from 'ionicons/icons';
 import React, { useState } from 'react';
-
+import './Records.css';
 
 interface Record {
     id: number;
@@ -12,19 +13,20 @@ const Records: React.FC = () =>
     const [records, setRecords] = useState<Record[]>([]);
     return (
         <IonPage>
-            <IonHeader>   
-            <IonTitle>Dossiers</IonTitle>
-            <IonToolbar>
-                <IonItem>   
-                        <IonSelect placeholder="Selectionnez une catégorie d'affaire">
-                        <IonSelectOption value="AllBusiness">Afficher affaires en cours et cloturées</IonSelectOption>
-                        <IonSelectOption value="OnGoingBusiness">Afficher affaires en cours</IonSelectOption>
-                        <IonSelectOption value="CompletedBusiness">Afficher affaires cloturées</IonSelectOption>
-                        </IonSelect>
-                    </IonItem>
-                    <IonItem>   
-                      
-                        <IonSearchbar></IonSearchbar>
+            <IonHeader>      
+                <IonToolbar>
+                    <IonItem>
+                        <IonTitle>Dossiers</IonTitle>
+                        <IonItem className='Business'>   
+                                <IonSelect placeholder="Selectionnez une catégorie d'affaire">
+                                <IonSelectOption value="AllBusiness">Afficher affaires en cours et cloturées</IonSelectOption>
+                                <IonSelectOption value="OnGoingBusiness">Afficher affaires en cours</IonSelectOption>
+                                <IonSelectOption value="CompletedBusiness">Afficher affaires cloturées</IonSelectOption>
+                                </IonSelect>
+                            </IonItem>
+                            <IonItem className='SearchBar'>   
+                                <IonSearchbar></IonSearchbar>
+                            </IonItem>
                     </IonItem>
                 </IonToolbar>
             </IonHeader>
@@ -35,7 +37,20 @@ const Records: React.FC = () =>
                         <IonCol>Code</IonCol>
                         <IonCol>Statut</IonCol>
                         <IonCol>Clients</IonCol>
-                        <IonCol>Actions</IonCol>
+                        <IonCol>Actions
+                            <IonToolbar>
+                                <IonButtons>
+                                    <IonButton >
+                                        <IonIcon slot="icon-only" icon={pencil} />
+                                    </IonButton>
+                                    <IonButton>
+                                        <IonIcon slot="icon-only" icon={trash} />
+                                    </IonButton>
+                                    </IonButtons>
+                                  
+                            </IonToolbar>
+                            
+                        </IonCol>
                     </IonRow>
                     <IonRow>
                         <IonCol>Code</IonCol>
