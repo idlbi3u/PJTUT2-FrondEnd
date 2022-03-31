8b35d719789cb98a1826a069319ca72935565d6a
Yann CALVET <71167181+CalvetYann@users.noreply.github.com>
2022-03-31 20:20:04 +0200

    Cases filter 2

--- src/pages/Records/Records.tsx
+++ src/pages/Records/Records.tsx
@@ -1,198 +1,144 @@
-import {
-    IonBackButton,
-    IonButton,
-    IonButtons,
-    IonCol,
-    IonContent,
-    IonGrid,
-    IonHeader,
-    IonIcon,
-    IonItem,
-    IonPage,
-    IonRow,
-    IonSearchbar,
-    IonSelect,
-    IonSelectOption,
-    IonTitle,
-    IonToolbar,
-    SearchbarChangeEventDetail
-} from '@ionic/react';
-import {addOutline, pencilOutline, pencilSharp, trashBinOutline, trashBinSharp} from 'ionicons/icons';
-import React, {useEffect, useState} from 'react';
+
+import { IonBackButton, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonPage, IonRoute, IonRow, IonSearchbar, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import {   addOutline, pencil,  trash,  } from 'ionicons/icons';
+import React, { useEffect, useState } from 'react';
 import './Records.css';
 import LawyercaseDataService from "../../services/lawyercase.service"
 import ILawyercase from '../../types/lawyercase.type';
+import AddRecord from '../../components/Dossier/AddRecord'
+import IClientData from '../../types/client.type';
 
 
-const Records: React.FC = () => {
+const Records: React.FC = () =>
+{
     const [isOpen, setIsOpen] = useState(false);
-    const [isEdit, setIsEdit] = useState(false);
-    const [selectedCase, setSelectedCase] = useState<ILawyercase>();
-    const [filter, setFilter] = useState<string>("all");
 
-    const handleDeleteCase = (id: string) => {
-        deleteCase(id);
-        window.location.reload();
+    const [filter, setFilter] = useState<string>("AllBusiness");
+
+    const handleDeleteRecord = (id: number) => {
+        
     }
 
-    const handleModifyCase = (case_: any) => {
-        console.log("case:")
-        console.log(case_)
+    const handleModifyRecord = (id: string) => {
+        deleteRecord(id);
     }
 
-    const [cases, setCases] = useState<ILawyercase[]>([]);
+    const [records, setRecords] = useState<ILawyercase[]>([]);
 
-    const retrieveCases = () => {
+    const retrieveRecords = () => {
         LawyercaseDataService.getAll()
             .then((response: any) => {
-                setCases(response.data)
+                setRecords(response.data)
                 console.log(response.data);
             })
             .catch((e: Error) => {
                 console.log(e);
             });
     }
-    const deleteCase = (id: string) => {
+    const deleteRecord = (id:string) => {
         LawyercaseDataService.delete(id)
-            .then((res: any) => {
+            .then((res:any) => {
                 console.log(res + "A bien été supprimé de la BDD");
-            })
-            .catch((e: Error) => {
+        })
+            .catch((e:Error) => {
                 console.log(e)
             })
     }
 
-    const handleSearchCases = async (e: CustomEvent<SearchbarChangeEventDetail>) => {
-
-        if (e.detail.value === "") {
-            retrieveCases()
-        }
-
-        await LawyercaseDataService.getAll()
-            .then((response: any) => {
-                setCases(response.data)
-            })
-            .catch((e: Error) => {
-                console.log(e);
-            });
-        if (e.detail.value) {
-            let tlc = e.detail.value.toLocaleLowerCase();
-            let filterData = cases.filter((e) => {
-                let refTlc = e.ref.toLocaleLowerCase();
-                return refTlc.indexOf(tlc) !== -1
-            })
-
-            setCases(filterData)
-        }
-
-    }
-
     useEffect(() => {
-        retrieveCases();
+        retrieveRecords();  
 
     }, []);
 
-    const [filteredCases, setFilteredCases] = useState<ILawyercase[]>(cases);
+    const [filteredRecords, setFilteredRecords] = useState<ILawyercase[]>(records);
 
     useEffect(() => {
-        if (filter === "all") {
-            setFilteredCases(cases);
-        } else if (filter === "open") {
-            setFilteredCases(cases.filter((e) => {
-                return !e.state;
-            }))
+        if (filter === "AllBusiness") {
+            setFilteredRecords(records);
+        } else if (filter === "OnGoingBusiness") {
+            setFilteredRecords(records.filter(record => !record.state));
         } else {
-            setFilteredCases(cases.filter((e) => {
-                return e.state;
-            }))
+            setFilteredRecords(records.filter(record => record.state));
         }
-    }, [filter, cases]);
-
-
+    });
+  
     return (
         <IonPage>
-            <IonHeader>
+            <IonHeader>      
                 <IonToolbar>
-                    <IonItem lines='none' slot='start'>
+                <IonItem lines='none' slot='start'>
                         <IonButtons slot='start'>
-                            <IonBackButton defaultHref='/home'/>
+                            <IonBackButton defaultHref='/home' ></IonBackButton>
                         </IonButtons>
-
-                        <IonTitle>Dossiers</IonTitle>
-                    </IonItem>
-                </IonToolbar>
-                <IonItem lines='none'>
-                    <IonItem slot='start'>
-                        <IonSelect value={filter} onIonChange={e => setFilter(e.detail.value)}>
-                            <IonSelectOption value="all">Tous</IonSelectOption>
-                            <IonSelectOption value="open">En cours</IonSelectOption>
-                            <IonSelectOption value="close">Clos</IonSelectOption>
-                        </IonSelect>
                     </IonItem>
                     <IonItem>
-                        <IonSearchbar
-                            onIonChange={(e) => handleSearchCases(e)}
-                            class='search-bar'
-                            type='text'
-                            placeholder="Rechercher par nom"/>
+                        <IonTitle>Dossiers</IonTitle>
+                        <IonItem className='Business' lines='none'>   
+                                <IonSelect placeholder="Selectionnez une catégorie d'affaire" value={filter} onIonChange={e => setFilter(e.detail.value)}>
+                                    <IonSelectOption value="AllBusiness">Toutes les affaires</IonSelectOption>
+                                    <IonSelectOption value="OnGoingBusiness">Affaires en cours</IonSelectOption>
+                                    <IonSelectOption value="CompletedBusiness">Affaires cloturées</IonSelectOption>
+                                </IonSelect>
+                            </IonItem>
+                            <IonItem className='SearchBar' lines='none'>   
+                                <IonSearchbar class='search-bar' type='text' animated={true}></IonSearchbar>
+                            </IonItem>
                     </IonItem>
-                </IonItem>
+
+                </IonToolbar>
             </IonHeader>
             <IonContent>
                 <IonItem lines='none'>
                     <IonButtons slot='end'>
-                        <IonButton onClick={() => {
-                            setIsOpen(true)
-                        }}>
-                            <IonIcon icon={addOutline}/>Ajouter
+                        <IonButton onClick={() => {setIsOpen(true)}} >
+                            <IonIcon icon={addOutline}></IonIcon>Ajouter
                         </IonButton>
                     </IonButtons>
                 </IonItem>
-
                 <IonGrid>
-                    <IonRow>
-                        <IonCol>Code</IonCol>
-                        <IonCol>Statut</IonCol>
-                        <IonCol>Clients</IonCol>
-                        <IonCol>Actions</IonCol>
+                    <IonRow className='Row'>
+                        <IonCol className='Col'>Code</IonCol>
+                        <IonCol className='Col'>Statut</IonCol>
+                        <IonCol className='Col'>Clients</IonCol>
+                        <IonCol className='Col'>Actions</IonCol>
                     </IonRow>
-                    {filteredCases.map((case_: ILawyercase, index: number) => {
+                    {filteredRecords.map((record: ILawyercase, index: number) => {
                         return (
-                            <IonRow key={index}>
-                                <IonCol>{case_.ref}</IonCol>
-                                <IonCol>{ case_.state ? "Clos" : "En cours" }</IonCol>
-                                <IonCol>Nom client</IonCol>
-                                <IonCol>
+                            <IonRow className='Row' key={index}>
+                                <IonCol className='Col'>{record.ref}</IonCol>
+                                <IonCol className='Col'>{record.state ? "Clôturé" : "En cours"}</IonCol>
+                                <IonCol className='Col'>
+                                    {record.clients ? record.clients.map((client: IClientData, index: number) => {
+                                        return (
+                                            client.name + " " + client.firstname + " / "
+                                        )
+                                    }) : "Aucun client"}
+                                </IonCol>
+                                <IonCol className='Col'>
                                     <IonButtons>
-                                        <IonButton onClick={() => {
-                                            handleDeleteCase(case_.id)
-                                        }}>
-                                            <IonIcon ios={trashBinOutline} md={trashBinSharp}/>
+                                        <IonButton >
+                                            <IonIcon slot="icon-only" icon={pencil} />
                                         </IonButton>
-                                        <IonButton onClick={() => {
-                                            handleModifyCase(case_)
-                                        }}>
-                                            <IonIcon ios={pencilOutline} md={pencilSharp}/>
+                                        <IonButton>
+                                            <IonIcon slot="icon-only" icon={trash} />
                                         </IonButton>
-                                    </IonButtons>
+                                    </IonButtons>             
                                 </IonCol>
                             </IonRow>
                         )
                     })}
                 </IonGrid>
             </IonContent>
-            {/* <AddCase isOpen={isOpen} setIsOpen={() => setIsOpen(false)}/> */}
-            {selectedCase ? (
-                <div>Oui</div>
-                // <EditCase
-                //     case={selectedCase}
-                //     isOpen={isEdit}
-                //     setIsOpen={() => setIsEdit(false)}
-                // />
-            ) : null}
-
-            )
-
+            <AddRecord isOpen={isOpen} setIsOpen={() => setIsOpen(false)}/>
+            <IonItem>
+                <IonButtons slot="end">
+                <IonButton className='Pages' color="black">Previous</IonButton>
+                <IonButton color="black">1</IonButton>
+                <IonButton color="black">2</IonButton>
+                <IonButton color="black">3</IonButton>
+                <IonButton className='Pages' color="black">Next</IonButton>
+                </IonButtons>
+            </IonItem>
         </IonPage>
     );
 }
