282e84500f1af7ae50912dc5a68119061744b178
Allan <allan.breuil@hotmail.fr>
2022-04-08 15:29:07 +0200

    Fix bug on search bar for lawyercases

--- src/http-common.ts
+++ src/http-common.ts
@@ -2,12 +2,16 @@ import axios from "axios";
 import {Capacitor} from "@capacitor/core";
 
 const env = Capacitor.getPlatform();
+
 let baseURL;
+
 if (env === "web") {
     baseURL = "http://localhost:8080/api"
 } else {
-    baseURL = "http://10.0.2.2/api"
+    baseURL = "http://10.0.2.2:8080/api"
 }
+
+console.log(baseURL);
 
 export default axios.create({
     baseURL: baseURL,
